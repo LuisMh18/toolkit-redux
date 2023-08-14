e45be8d05c9a1a4180e500fec19265544d11f56d
Luis Ángel Herrera <luis_mh@outlook.es>
2023-08-14 05:22:40 -0600

    Thunks, axios

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { store } from './store';
 import { Provider } from 'react-redux'
-import App from './App.jsx';
 import './index.css';
+import { PokemonApp } from '../PokemonApp';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={ store }>
-      <App />
+      <PokemonApp />
     </Provider>
   </React.StrictMode>,
 )
